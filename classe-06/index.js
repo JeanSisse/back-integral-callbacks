364c2f6e6d2de571b8493f79d93aa71a08dba277
JeanSisse <tavares1988@live.com>
2021-06-29 18:49:50 -0300

    Exercicio 'Temporizador de jogos' resolvido

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,1 +1,21 @@
 const jogadores = ['Guido', 'Dina', 'Léo', 'Nanda', 'Juninho'];
+
+const tempo = 10;
+let index = -1;
+let tempoEsgotado = 0;
+
+function  imprimirVez(){
+	
+	if (tempoEsgotado === tempo){
+		console.log(`A rodada terminou!`);
+		clearInterval(intervalID);
+	} else {
+		console.log(jogadores[++index]);
+	}
+
+	tempoEsgotado += tempo/jogadores.length;	
+}
+
+imprimirVez();
+
+const intervalID = setInterval(imprimirVez, tempo/jogadores.length*1000);
