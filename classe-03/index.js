ff6887e8f6ca69030da2606ec959759bbd970b2e
JeanSisse <tavares1988@live.com>
2021-06-29 17:04:11 -0300

    Exercicio 'Modificando um array' resolvido

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,1 +1,7 @@
 const frutas = ["Manga", "UVA", "abacaxi", "banaNA", "MAçã"];
+
+const frutasNumerado = frutas.map((fruta, index) => {
+	return `${index} - ${fruta}`
+});
+
+console.log(frutasNumerado);
