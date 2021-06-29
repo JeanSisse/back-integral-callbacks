e6f7613ce18e3a98f4dcac4670f5934ff2a40b69
JeanSisse <tavares1988@live.com>
2021-06-29 16:35:25 -0300

    Exercicio 'Busca de CEP' resolvido

--- classe-02/index.js
+++ classe-02/index.js
@@ -4,3 +4,10 @@ const endereços = [
     { cep: 00222444, rua: "Avenida Paralela" },
     { cep: 11222333, rua: "Rua Carlos Gomes" },
 ];
+
+function encontre_endereco(cep){
+    const ruaFiltrado = endereços.find(x => x.cep === cep);
+    return ruaFiltrado.rua;
+}
+
+console.log(encontre_endereco(00222444));
