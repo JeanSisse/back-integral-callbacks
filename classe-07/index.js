f263db20f3b6b78c7dfccd0483e62f32417e9bba
JeanSisse <tavares1988@live.com>
2021-06-29 19:04:45 -0300

    Exercicio 'Escrevendo em arquivo' resolvido

--- classe-07/index.js
+++ classe-07/index.js
@@ -1,1 +1,7 @@
-const jogadores = ['Guido', 'Dina', 'Léo', 'Nanda', 'Juninho'];
+const fs = require('fs');
+const msg = "Estou aprendendo JavaScript na Cubos Academy";
+
+fs.writeFile("meuarquivo.txt", msg, () => {
+	console.log("Arquivo foi escrito com sucesso.");
+	// console.log(fs.readFileSync('meuarquivo.txt').toString());
+});
